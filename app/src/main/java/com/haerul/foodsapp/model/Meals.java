eca1fe438ffb26a5359cdaed90546d41557dd831
trunghieule0301 <trunghieule0301@gmail.com>
2020-06-19 12:23:07 +0700

    điền vào các Todo để hiện thực app nhe mọi người

---
+++ app/src/main/java/com/haerul/foodsapp/model/Meals.java
@@ -0,0 +1,36 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 3/17/19 5:24 AM                                              -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2019. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.haerul.foodsapp.model;
+
+public class Meals {
+
+    /*
+     * TODO 8 Paste the generated results
+     *
+     * Paste the generated results from -
+     * http://www.jsonschema2pojo.org/
+     */
+
+    public class Meal {
+        public String getStrMealThumb() {
+            String str1 = "Welcome";
+            return str1;
+        }
+
+        public String getStrMeal() {
+            String str1 = "Welcome";
+            return str1;
+        }
+
+        /*
+         * TODO 9 Paste the generated results
+         *
+         * Paste the generated results Level 2 (Meal Item) from -
+         * http://www.jsonschema2pojo.org/
+         */
+    }
+}
